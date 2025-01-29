207eae5960e269b1a0485002779d5f2c92325d77
Kevin Maurizi <kevin.maurizi@bzz.ch>
2025-01-29 08:53:02 +0100

    added solution

--- efuel.py
+++ efuel.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from entry import Entry
+
+
 def main():
     """
     Main function to run the application.
@@ -10,37 +15,58 @@ def main():
 
 def init_accounts():
     
-    return None
+    return {'Inga':[],'Hans':[],'Samuel':[]}
 
 
-def add_entries():
-    pass
+def add_entries(accounts):
+    end = ''
+    while end != 'y':
+        name = input('Benutzer:')
+        accounts[name].append(read_entry())
+        end = input('Beenden? (y/N)')
 
 
 def read_entry():
-    return None
+    start = read_datetime('Start')
+    end = read_datetime('End')
+    free = read_datetime('Free')
+    energy = read_float('Energy')
+    return Entry(start,end,free,energy)
 
 
-def show_balance():
 
-    # Basisversion: Zeile mit Datum und Kosten
-    # print(f'  - {DATUM}\tCHF {KOSTEN:.2f}')
-
-    # Erweitert: Zeile mit Datum und Kosten
-    # print(f'  - {DATUM:%d.%m.%Y %H:%M}\tCHF {KOSTEN:.2f}')
-
-    # Beide Versionen: Zeile mit Total
-    # print(f'{"Total:":<20}\tCHF {TOTAL:.2f}')
-
-    pass
+def show_balance(accounts):
+    for name, entries in accounts.items():
+        print(f'Abrechnung für {name}')
+        total = 0
+        for entry in entries:
+            total += entry.cost
+            print(f'  - {entry.start:%d.%m.%Y %H:%M}\tCHF {entry.cost:.2f}')
+        print(f'{"Total:":<20}\tCHF {total:.2f}')
 
 
-def read_float():
-    return 13.79
+def read_float(prompt):
+    min_value = 0
+    while True:
+        try:
+            zahl = float(input(prompt))
+        except ValueError:
+            print('Geben Sie eine positive Zahl ein')
+        else:
+            if zahl < min_value:
+                print(f'Geben Sie eine positive Zahl ein')
+            else:
+                return zahl
 
 
-def read_datetime():
-    return '01.01.2099 00:01'
+def read_datetime(prompt):
+    while True:
+        try:
+            date_input = input(prompt)
+            date_time = datetime.strptime(date_input, '%d.%m.%Y %H:%M')
+            return date_time
+        except ValueError:
+            print('Geben Sie ein gültiges Datum/Uhrzeit ein')
 
 
 if __name__ == '__main__':
--- entry.py
+++ entry.py
@@ -1,3 +1,15 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
 class Entry:
+    start:datetime
+    end:datetime
+    free:datetime
+    energy:float
+
+    @property
     def cost(self):
-        return 1.57
+        parktime = max(int((self.free-self.end).seconds/60)-15,0)
+        return round(self.energy*0.31+parktime*0.05,2)
